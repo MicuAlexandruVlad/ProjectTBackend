249eb637788820665eaca0dbd7fda9f058a04374
Micu Alexandru-Vlad <vlad15235@gmail.com>
2024-07-01 00:36:37 +0300

    Updating the user's stats in the cache when they create a new post

--- src/routes/post/post.ts
+++ src/routes/post/post.ts
@@ -3,13 +3,30 @@ import { PostRoutes } from "../RouteConstants";
 import { useProtectedRoute } from "../protectedRoute";
 import { UnuploadedPost } from "../../data/models/bodyData/Post";
 import { createPost, getUserPosts } from "../../data/neo4j/posts";
+import { incrementCacheValue } from "../../data/redis/cache";
+import { getPostsKey } from "../../data/redis/constants";
 
 const app = new Hono()
 
 app.post(PostRoutes.CREATE, (c, next) => useProtectedRoute(c, next), async (c) => {
     const unuploadedPost = await c.req.json<UnuploadedPost>()
 
-    const post = await createPost(unuploadedPost)
+    const post = await createPost(unuploadedPost).catch(err => {
+        console.log('Error creating post', err)
+
+        return c.json({
+            message: 'Error creating post'
+        })
+    
+    })
+
+    await incrementCacheValue(getPostsKey(unuploadedPost.userId)).catch(err => {
+        console.log('Error incrementing cache value', err)
+
+        return c.json({
+            message: 'Error creating post'
+        })
+    })
 
     return c.json({
         message: 'Post created',
